5a5aaa66cc3f89681fcf72eeb709bae08e1af09c
P@terne <61385212+teamflp@users.noreply.github.com>
2023-08-07 09:46:39 +0200

    Update crud.php

--- 08_mysql/crud.php
+++ 08_mysql/crud.php
@@ -1,17 +1,19 @@
 <!DOCTYPE html>
 <html lang="fr">
 <head>
-     <title>Apprendre à coder en php: La clause Insert</title>
+     <title>Apprendre à coder en php: CRUD</title>
 </head>
 <body>
 
      <?php 
+     // La clause INSERT
      $sql = "INSERT INTO table_name (column1, column2) VALUES (?, ?)";
      $stmt = $conn->prepare($sql);
      $stmt->execute([$value1, $value2]);
      ?>
 
      <?php 
+     // La clause SELECT
      $sql = "SELECT column1, column2 FROM table_name";
      $stmt = $conn->prepare($sql);
      $stmt->execute();
@@ -19,12 +21,14 @@
      ?>
 
      <?php 
+     // La clause UPDATE
      $sql = "UPDATE table_name SET column1 = ? WHERE column2 = ?";
      $stmt = $conn->prepare($sql);
      $stmt->execute([$new_value, $value]);
      ?>
 
      <?php 
+     // La clause DELETE
      $sql = "DELETE FROM table_name WHERE column = ?";
      $stmt = $conn->prepare($sql);
      $stmt->execute([$value]);
