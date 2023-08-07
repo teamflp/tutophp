ab882d0bd95f4bf865997a8cdbe746091ee194b8
teamflp <teamflp@yahoo.it>
2023-08-07 23:22:54 +0200

    Ajout des fichiers au dossier 09_poo

--- 04_conditions/01_if.php
+++ 04_conditions/01_if.php
@@ -1,18 +1,21 @@
 <!DOCTYPE html>
 <html lang="fr">
 <head>
-     <title>Apprendre à coder en php: stuctures de controle - if</title>
+     <title>Apprendre à coder en php: structures de controle  if</title>
 </head>
 <body>
-     
-     <?php
+
+/** @noinspection ALL */<?php
      // if est une instruction qui permet de faire des tests
      echo "<h1>L'instruction if</h1>";
      $age = 25;
 
      if ($age >= 18) {
-          echo "Vous êtes majeur.";
+         echo "Vous êtes majeur.";
+     } else {
+         echo "Vous êtes mineur.";
      }
+
      ?>
 
 </body>
